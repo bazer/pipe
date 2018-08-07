7d392061f1ec9f28328d08b5018b2809e3d879f1
Sebastian Öberg <sebastian.oberg@gmail.com>
2018-08-07 19:25:13 +0200

    Added a simple test and updated packages

--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -2,10 +2,26 @@ import { assert } from 'chai';
 import Parser from '../src/reader/parser';
 
 describe('Parser test suite', function() {
-  it('can handle empty string', function() {
+  it('can parse empty string', function() {
     let parser = new Parser("");
     let result = parser.parse();
 
     assert.isEmpty(result);
+  });
+
+  it('can parse simple text', function() {
+    let parser = new Parser("Simple text");
+    let result = parser.parse();
+
+    assert.isNotEmpty(result);
+    assert.equal(result.length, 1);
+    assert.equal(result[0].name, "t");
+    assert.equal(result[0].children.length, 3);
+    assert.equal(result[0].children[0].name, "w");
+    assert.equal(result[0].children[0].value, "Simple");
+    assert.equal(result[0].children[1].name, "_");
+    assert.equal(result[0].children[1].amount, 1);
+    assert.equal(result[0].children[2].name, "w");
+    assert.equal(result[0].children[2].value, "text");
   });
 });
